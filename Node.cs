c45cf1adb90987708980dc36691e733f21fd7fcf
Kunal Relan <relan.krunal@gmail.com>
2022-01-27 16:20:39 +0530

    Add files via upload

---
+++ Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Binarytree
+{
+    public class Node
+    {
+        public Node left;
+        public Node right;
+
+        public int NodeValue { get; }
+
+        public Node(int value)
+        {
+            this.NodeValue = value;
+        }
+
+        public int Count(Node root)
+        {
+            if (root == null)
+                return 0;
+            return 1 + Count(root.left) + Count(root.right);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Binarytree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node objNode = new Node(1);
+            objNode.left = new Node(2)
+            {
+                left = new Node(5),
+                right = new Node(5)
+            };
+
+            objNode.right = new Node(3);
+
+            Console.WriteLine($"node count {objNode.Count(objNode)}");
+
+            Console.WriteLine($"binary tree:{objNode}");
+            Console.WriteLine();
+        }
+    }
+}
